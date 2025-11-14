2a2074089946ebdfd6462c76d9e53b9ed8198767
vanessitax333x <vanessapm12321@gmail.com>
2025-11-14 08:04:36 -0600

    vanessitax333x.py
    #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO en Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/vanessitax333x.py
@@ -0,0 +1,91 @@
+
+
+#https://www.python.org/
+
+# <--- Signo para crear un Comentario en una Línea (Almohadilla, Numeral, Gato, etc.)
+
+#Para crear un Comentario en Varias Líneas se necesitan de 3 pares de Comillas ó 3 Comillas Simples al inicio del comentario y al final como se Muestra a Continuación("""  ó  ''')
+
+"""
+Esto también se marcara 
+como comentario debido a 
+que se colocan los 3 
+pares de comillas al inicio y 
+al final como se muestra
+"""
+
+#ó
+
+'''
+Esto también se marcara 
+como comentario debido a 
+que se colocan los 3 
+pares de comillas al inicio y 
+al final como se muestra
+'''
+
+my_variable = "Mi variable"    #<---   Variable de tipo Cadena de Texto o String (Las Variables de tipo texto o String tienen la particularidad de ir siempre entre Comillas)
+my_variable = "Nuevo Valor de mi Variable"   #<---  #Es posible actualizar el valor de una variable simplemente sobreescribiendola de esta manera
+
+MY_CONSTANT = "Mi Constante"    #<---   Al momento de que se crea una variable con mayúsculas, lo que se quiere representar es el hecho de no querer que se modifique su valor, aunque  se pueda cambiar  // Por Convención
+MY_CONSTANT = "sdaksjdnaknc"    #<---   Representan Constantes
+
+#Existen 4 tipos de datos primitivos
+int
+float
+str
+bool
+
+
+
+
+
+
+my_int: int = 10     #<--- Se utiliza para hacer mas Explícito el tipo de dato Deseado
+my_int=10            #<--- int o integer representan números enteros
+my_float=13.6        #<--- float representan números con decimales
+my_str="Hola"        #<--- str o string representan texto con comillas dobles o simples 
+my_bool=True         #<--- bool representa verdadero o falso
+my_bool=False        #<--- bool representa verdadero o falso
+
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_str))
+print(type(my_bool))
+print(type(my_bool))
+
+
+#En Python se utiliza "Print()" para mostrar valores en Consola como se muestra a Continuación
+
+print("¡Hola, Python!")
+
+
+
+
+
+
+"""
+¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
